6d7126e7c51cf4d0ead816c1b17af8569e7f6daa
Daniel Tull <dt@danieltull.co.uk>
2012-05-25 12:35:46 +0100

    Initial commit

---
+++ DCTImageCache.h
@@ -0,0 +1,13 @@
+//
+//  DCTImageCache.h
+//  Tweetville
+//
+//  Created by Daniel Tull on 25.05.2012.
+//  Copyright (c) 2012 Daniel Tull Limited. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DCTImageCache : NSObject
+
+@end
---
+++ DCTImageCache.m
@@ -0,0 +1,13 @@
+//
+//  DCTImageCache.m
+//  Tweetville
+//
+//  Created by Daniel Tull on 25.05.2012.
+//  Copyright (c) 2012 Daniel Tull Limited. All rights reserved.
+//
+
+#import "DCTImageCache.h"
+
+@implementation DCTImageCache
+
+@end
