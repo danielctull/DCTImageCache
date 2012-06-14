f9f906b6888e713ffe5e52877cfbf43af9720c52
Daniel Tull <dt@danieltull.co.uk>
2012-06-14 14:30:05 +0100

    Add static library project

---
+++ DCTImageCacheTests/DCTImageCacheTests.h
@@ -0,0 +1,13 @@
+//
+//  DCTImageCacheTests.h
+//  DCTImageCacheTests
+//
+//  Created by Daniel Tull on 14.06.2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DCTImageCacheTests : SenTestCase
+
+@end
---
+++ DCTImageCacheTests/DCTImageCacheTests.m
@@ -0,0 +1,32 @@
+//
+//  DCTImageCacheTests.m
+//  DCTImageCacheTests
+//
+//  Created by Daniel Tull on 14.06.2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import "DCTImageCacheTests.h"
+
+@implementation DCTImageCacheTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DCTImageCacheTests");
+}
+
+@end
