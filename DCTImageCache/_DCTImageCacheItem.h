9a9e97d42e8bbffdccb851a6664bb9ade5feaca8
Daniel Tull <dt@danieltull.co.uk>
2012-12-02 17:15:40 +0000

    Remove uneeded file

--- DCTImageCache/_DCTImageCacheItem.h
+++ DCTImageCache/_DCTImageCacheItem.h
@@ -1,5 +1,105 @@
-#import "__DCTImageCacheItem.h"
+// DO NOT EDIT. This file is machine-generated and constantly overwritten.
+// Make changes to _DCTImageCacheItem.h instead.
 
-@interface _DCTImageCacheItem : __DCTImageCacheItem {}
-// Custom logic goes here.
+#import <CoreData/CoreData.h>
+
+
+extern const struct _DCTImageCacheItemAttributes {
+	__unsafe_unretained NSString *date;
+	__unsafe_unretained NSString *imageData;
+	__unsafe_unretained NSString *key;
+	__unsafe_unretained NSString *sizeString;
+} _DCTImageCacheItemAttributes;
+
+extern const struct _DCTImageCacheItemRelationships {
+} _DCTImageCacheItemRelationships;
+
+extern const struct _DCTImageCacheItemFetchedProperties {
+} _DCTImageCacheItemFetchedProperties;
+
+
+
+
+
+
+
+@interface _DCTImageCacheItemID : NSManagedObjectID {}
+@end
+
+@interface _DCTImageCacheItem : NSManagedObject {}
++ (id)insertInManagedObjectContext:(NSManagedObjectContext*)moc_;
++ (NSString*)entityName;
++ (NSEntityDescription*)entityInManagedObjectContext:(NSManagedObjectContext*)moc_;
+- (_DCTImageCacheItemID*)objectID;
+
+
+
+
+@property (nonatomic, strong) NSDate* date;
+
+
+//- (BOOL)validateDate:(id*)value_ error:(NSError**)error_;
+
+
+
+
+@property (nonatomic, strong) NSData* imageData;
+
+
+//- (BOOL)validateImageData:(id*)value_ error:(NSError**)error_;
+
+
+
+
+@property (nonatomic, strong) NSString* key;
+
+
+//- (BOOL)validateKey:(id*)value_ error:(NSError**)error_;
+
+
+
+
+@property (nonatomic, strong) NSString* sizeString;
+
+
+//- (BOOL)validateSizeString:(id*)value_ error:(NSError**)error_;
+
+
+
+
+
+
+@end
+
+@interface _DCTImageCacheItem (CoreDataGeneratedAccessors)
+
+@end
+
+@interface _DCTImageCacheItem (CoreDataGeneratedPrimitiveAccessors)
+
+
+- (NSDate*)primitiveDate;
+- (void)setPrimitiveDate:(NSDate*)value;
+
+
+
+
+- (NSData*)primitiveImageData;
+- (void)setPrimitiveImageData:(NSData*)value;
+
+
+
+
+- (NSString*)primitiveKey;
+- (void)setPrimitiveKey:(NSString*)value;
+
+
+
+
+- (NSString*)primitiveSizeString;
+- (void)setPrimitiveSizeString:(NSString*)value;
+
+
+
+
 @end
--- DCTImageCache/_DCTImageCacheItem.m
+++ DCTImageCache/_DCTImageCacheItem.m
@@ -1,7 +1,85 @@
+// DO NOT EDIT. This file is machine-generated and constantly overwritten.
+// Make changes to _DCTImageCacheItem.m instead.
+
 #import "_DCTImageCacheItem.h"
+
+const struct _DCTImageCacheItemAttributes _DCTImageCacheItemAttributes = {
+	.date = @"date",
+	.imageData = @"imageData",
+	.key = @"key",
+	.sizeString = @"sizeString",
+};
+
+const struct _DCTImageCacheItemRelationships _DCTImageCacheItemRelationships = {
+};
+
+const struct _DCTImageCacheItemFetchedProperties _DCTImageCacheItemFetchedProperties = {
+};
+
+@implementation _DCTImageCacheItemID
+@end
 
 @implementation _DCTImageCacheItem
 
-// Custom logic goes here.
++ (id)insertInManagedObjectContext:(NSManagedObjectContext*)moc_ {
+	NSParameterAssert(moc_);
+	return [NSEntityDescription insertNewObjectForEntityForName:@"DCTImageCacheItem" inManagedObjectContext:moc_];
+}
+
++ (NSString*)entityName {
+	return @"DCTImageCacheItem";
+}
+
++ (NSEntityDescription*)entityInManagedObjectContext:(NSManagedObjectContext*)moc_ {
+	NSParameterAssert(moc_);
+	return [NSEntityDescription entityForName:@"DCTImageCacheItem" inManagedObjectContext:moc_];
+}
+
+- (_DCTImageCacheItemID*)objectID {
+	return (_DCTImageCacheItemID*)[super objectID];
+}
+
++ (NSSet *)keyPathsForValuesAffectingValueForKey:(NSString *)key {
+	NSSet *keyPaths = [super keyPathsForValuesAffectingValueForKey:key];
+	
+
+	return keyPaths;
+}
+
+
+
+
+@dynamic date;
+
+
+
+
+
+
+@dynamic imageData;
+
+
+
+
+
+
+@dynamic key;
+
+
+
+
+
+
+@dynamic sizeString;
+
+
+
+
+
+
+
+
+
+
 
 @end
